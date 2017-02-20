ef01c223773b8d2e42c0a1ca6761996b696f81b3
poignant.wit <poignant.wit@gmail.com>
2017-02-21 01:07:34 +0200

    feat (utils): add string util

---
+++ src/utils/string.js
@@ -0,0 +1,11 @@
+import R from 'ramda'; 
+
+export const getSinceFromLinkHeader = R.pipe(
+    R.split(','),
+    R.head,
+    R.split(';'),
+    R.head,
+    R.split('='),
+    R.last,
+    R.dropLast(1)
+)
